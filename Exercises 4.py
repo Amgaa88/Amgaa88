f5b21dbdc325a2da769133e23e8737b5358cea8f
Jiigee <104887482+Amgaa88@users.noreply.github.com>
2022-05-04 14:50:20 +0800

    Add files via upload

---
+++ Exercises 4.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+x=0
+while (x<5):
+    print('hi '*x)
+    x+=1 
+
+
+# In[ ]:
+
+
+#For loop exercises
+
+
+# In[16]:
+
+
+for i in 'blood-oxygenation level dependent functional magnetic resonance imaging':
+    print(i)
+
+
+# In[18]:
+
+
+grocery_listt=['candy','flour','watermelon','apple','orange']
+for i in grocery_listt:
+    print('Note to self, buy: '+i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
